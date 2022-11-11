fb436f6e6851502211522c24aaf2f587762a14c1
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2022-11-11 20:49:21 +0100

    bump: version 0.1.0 → 0.2.0

--- cutepandas/__init__.py
+++ cutepandas/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Philipp Temminghoff"""
 __email__ = "phil65@kodi.tv"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 __all__ = [
     "pandaswidgets",
---
+++ docs/changelog.md
@@ -0,0 +1,5 @@
+## v0.2.0 (2022-11-11)
+
+### Feat
+
+- initial commit
